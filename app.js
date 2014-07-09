40bb8bfc8da7c222f57ae52bfafe4d6d863b4465
chrisalexander <chris.alexander@import.io>
2014-07-09 16:13:15 +0100

    Turns out CSV export is quite tricky

--- app.js
+++ app.js
@@ -71,6 +71,64 @@ angular.module("seo").controller("SEO", function($scope, $timeout, safeApply, io
 		});
 	}
 
+	$scope.openCSV = function() {
+		var data = {};
+		var checkFieldInitialised = function(heading) {
+			if (!data.hasOwnProperty(heading)) {
+				data[heading] = [];
+			}
+		}
+		var normaliseDataLengths = function() {
+			var maxLength = 0;
+			for (var k in data) {
+				if (data[k].length > maxLength) {
+					maxLength = data[k].length;
+				}
+			}
+			for (var k in data) {
+				while(data[k].length < maxLength) {
+					data[k].push("");
+				}
+			}
+		}
+		var enquote = function(input) {
+			if (!input) {
+				return "";
+			}
+			if (Object.prototype.toString.call(input) === '[object Array]') {
+				input = input.join("; ");
+			}
+			input = input.replace(/,/g, "\\,");
+			if (input.indexOf(",") >= 0 || input.indexOf("\n") >= 0 || input.indexOf('"') >= 0 || input.indexOf(';') >= 0) {
+				return '"' + input.replace(/"|“|”/ig, '""').replace(/\n|\r/ig, "") + '"';
+			}
+			return input;
+		}
+		$scope.rows.map(function(row) {
+			checkFieldInitialised("url");
+			for (var k in row.data) {
+				checkFieldInitialised(k);
+			}
+			
+			normaliseDataLengths();
+			data.url.push(row.url);
+			for (var k in row.data) {
+				data[k].push(row.data[k]);
+			}
+		});
+		var csv = "";
+		var headings = Object.keys(data);
+		csv += headings.map(enquote).join(",") + "\n";
+		for (var i = 0; i < data.url.length; i++) {
+			var row = [];
+			for (var j = 0; j < headings.length; j++) {
+				row.push(data[headings[j]][i]);
+			}
+			csv += row.map(enquote).join(",") + "\n";
+		}
+		window.open("data:application/csv;charset=utf-8," + encodeURIComponent(csv));
+	}
+
 	var collapseAllRows = function() {
 		$scope.rows.map(function(row) {
 			row.expanded = false;
