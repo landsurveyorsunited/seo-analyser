dd85f0ab5a55b3a8e30d0cc1831797290733e17e
chrisalexander <chris.alexander@import.io>
2014-07-09 12:39:49 +0100

    First retrieval of URLs

---
+++ app.js
@@ -0,0 +1,76 @@
+angular.module("seo", ["importio"]);
+
+importio.init({
+	"auth": {
+		"userGuid": "e4aa58c3-bb28-4468-87a0-d574f4dbc778",
+		"apiKey": "Ayv5OkcvJMFqqXchUlvYPuSGAv3bO7BAMsa2lv0+hLN3DJ3aA/hS/8uPB2p0mYUld1PcFUZA2JUNhMDg9h4cqw=="
+	}
+});
+
+angular.module("seo").controller("SEO", function($scope, safeApply, ioquery) {
+
+	$scope.inputUrl = "https://import.io";
+
+	$scope.rows = [];
+
+	$scope.refreshRow = function(row) {
+		if (row.loading) {
+			return;
+		}
+		row.loading = true;
+		ioquery.query({
+			"connectorGuids": ["7f8a9f9f-f7f4-4ea2-8af7-477c28af3e62"],
+			"input": {
+				"webpage/url": row.url
+			}
+		}).then(function(data) {
+			row.data = data[0].data;
+			row.loading = false;
+			row.failed = false;
+			safeApply($scope);
+		}, function() {
+			row.data = false;
+			row.loading = false;
+			row.failed = true;
+			safeApply($scope);
+		});
+	}
+
+	$scope.addUrl = function(inputUrl) {
+		for (var i = 0; i < $scope.rows.length; i++) {
+			if ($scope.rows[i].url == inputUrl) {
+				$scope.inputUrl = "";
+				return;
+			}
+		}
+		var newRow = {
+			"url": inputUrl,
+			"data": false,
+			"loading": false,
+			"expanded": false,
+			"failed": false
+		}
+		$scope.rows.unshift(newRow);
+		$scope.refreshRow(newRow);
+		$scope.inputUrl = "";
+	}
+
+});
+
+angular.module("seo").factory("safeApply", function($rootScope) {
+
+	return function($scope) {
+		try {
+			if (!$scope) {
+				console.error("safeApply not applied as no scope was provided");
+				return;
+			}
+			if ($scope.$root.$$phase != "$apply" && $scope.$root.$$phase != "$digest") {
+				$scope.$apply();
+			}
+		} catch (e) {
+			console.error("safeApply threw error", e);
+		}
+	}
+
+});
